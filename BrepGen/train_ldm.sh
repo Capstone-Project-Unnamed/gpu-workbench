d6bb2b803fed1f006f09722da41472f9c4a64132
juyeonnnn <opff5423@gmail.com>
2026-07-06 15:16:41 +0900

    fix: update training script path for partABC dataset

--- BrepGen/train_ldm.sh
+++ BrepGen/train_ldm.sh
@@ -1,82 +1,32 @@
-#!/bin/bash\
+#!/bin/bash
 
-### Train the Latent Diffusion Model ###
-# --data_aug is optional
-# max_face 30, max_edge 20 for deepcad 
-# max_face 50, max_edge 30 for abc/furniture
-# --surfvae refer to the surface vae weights 
-# --edgevae refer to the edge vae weights 
+### Training partABC (001) Latent Diffusion Model ### 
+# 경로: /mnt/sdb/TMEMJ/partABC_data/parsed_001
+# 이미지 경로: /mnt/sdb/TMEMJ/partABC_data/abc_images
 
-### Training DeepCAD Latent Diffusion Model ###  
-python ldm.py --data data_process/deepcad_parsed \
-    --list data_process/deepcad_data_split_6bit.pkl --option surfpos --gpu 0 1 \
-    --env deepcad_ldm_surfpos --train_nepoch 3000 --test_nepoch 200 --save_nepoch 200 \
-    --max_face 30 --max_edge 20
-
-python ldm.py --data data_process/deepcad_parsed \
-    --list data_process/deepcad_data_split_6bit.pkl --option surfz \
-    --surfvae proj_log/deepcad_vae_surf.pt --gpu 0 1 \
-    --env deepcad_ldm_surfz --train_nepoch 3000 --batch_size 256 \
-    --max_face 30 --max_edge 20
-
-python ldm.py --data data_process/deepcad_parsed \
-    --list data_process/deepcad_data_split_6bit.pkl --option edgepos \
-    --surfvae proj_log/deepcad_vae_surf.pt --gpu 0 1 \
-    --env deepcad_ldm_edgepos --train_nepoch 1000 --batch_size 128 \
-    --max_face 30 --max_edge 20
-
-python ldm.py --data data_process/deepcad_parsed \
-    --list data_process/deepcad_data_split_6bit.pkl --option edgez \
-    --surfvae proj_log/deepcad_vae_surf.pt --edgevae proj_log/deepcad_vae_edge.pt --gpu 0 1 \
-    --env deepcad_ldm_edgez --train_nepoch 1000 --batch_size 128 \
-    --max_face 30 --max_edge 20
-
-
-### Training ABC Latent Diffusion Model ###  
-python ldm.py --data data_process/abc_parsed \
-    --list data_process/abc_data_split_6bit.pkl --option surfpos --gpu 0 1 \
-    --env abc_ldm_surfpos --train_nepoch 1000 --test_nepoch 200 --save_nepoch 200 \
+# 1. Surface Position 훈련
+python ldm.py --data /mnt/sdb/TMEMJ/partABC_data/parsed_001 \
+    --list /mnt/sdb/TMEMJ/partABC_data/parsed_001/split.pkl --option surfpos --gpu 0 1 \
+    --env partABC_ldm_surfpos --train_nepoch 1000 --test_nepoch 200 --save_nepoch 200 \
     --max_face 50 --max_edge 30
 
-python ldm.py --data data_process/abc_parsed \
-    --list data_process/abc_data_split_6bit.pkl --option surfz \
+# 2. Surface Z 훈련 (VAE 가중치 포함)
+python ldm.py --data /mnt/sdb/TMEMJ/partABC_data/parsed_001 \
+    --list /mnt/sdb/TMEMJ/partABC_data/parsed_001/split.pkl --option surfz \
     --surfvae proj_log/abc_vae_surf.pt --gpu 0 1 \
-    --env abc_ldm_surfz --train_nepoch 1000 --batch_size 256 \
+    --env partABC_ldm_surfz --train_nepoch 1000 --batch_size 256 \
     --max_face 50 --max_edge 30
 
-python ldm.py --data data_process/abc_parsed \
-    --list data_process/abc_data_split_6bit.pkl --option edgepos \
+# 3. Edge Position 훈련
+python ldm.py --data /mnt/sdb/TMEMJ/partABC_data/parsed_001 \
+    --list /mnt/sdb/TMEMJ/partABC_data/parsed_001/split.pkl --option edgepos \
     --surfvae proj_log/abc_vae_surf.pt --gpu 0 1 \
-    --env abc_ldm_edgepos --train_nepoch 300 --batch_size 64 \
+    --env partABC_ldm_edgepos --train_nepoch 300 --batch_size 64 \
     --max_face 50 --max_edge 30
 
-python ldm.py --data data_process/abc_parsed \
-    --list data_process/abc_data_split_6bit.pkl --option edgez \
+# 4. Edge Z 훈련 (VAE 가중치 포함)
+python ldm.py --data /mnt/sdb/TMEMJ/partABC_data/parsed_001 \
+    --list /mnt/sdb/TMEMJ/partABC_data/parsed_001/split.pkl --option edgez \
     --surfvae proj_log/abc_vae_surf.pt --edgevae proj_log/abc_vae_edge.pt --gpu 0 1 \
-    --env abc_ldm_edgez --train_nepoch 300 --batch_size 64 \
+    --env partABC_ldm_edgez --train_nepoch 300 --batch_size 64 \
     --max_face 50 --max_edge 30
-
-
-### Training Furniture Latent Diffusion Model (classifier-free) ###  
-python ldm.py --data data_process/furniture_parsed \
-    --list data_process/furniture_data_split_6bit.pkl --option surfpos --gpu 0 1 \
-    --env furniture_ldm_surfpos --train_nepoch 3000 --test_nepoch 200 --save_nepoch 200 \
-    --max_face 50 --max_edge 30 --cf
-
-python ldm.py --data data_process/furniture_parsed \
-    --list data_process/furniture_data_split_6bit.pkl --option surfz \
-    --surfvae proj_log/furniture_vae_surf.pt --gpu 0 1 \
-    --env furniture_ldm_surfz --train_nepoch 3000 --batch_size 256 \
-    --max_face 50 --max_edge 30 --cf
-
-python ldm.py --data data_process/furniture_parsed \
-    --list data_process/furniture_data_split_6bit.pkl --option edgepos \
-    --surfvae proj_log/furniture_vae_surf.pt --gpu 0 1 \
-    --env furniture_ldm_edgepos --train_nepoch 1000 --batch_size 64 \
-    --max_face 50 --max_edge 30 --cf
-
-python ldm.py --data data_process/furniture_parsed \
-    --list data_process/furniture_data_split_6bit.pkl --option edgez \
-    --surfvae proj_log/furniture_vae_surf.pt --edgevae proj_log/furniture_vae_edge.pt --gpu 0 1 \
-    --env furniture_ldm_edgez --train_nepoch 1000 --batch_size 64 \
-    --max_face 50 --max_edge 30 --cf
